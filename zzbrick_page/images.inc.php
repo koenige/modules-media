33586c4e91d48dc53ca460627f3407944303b336
Gustaf Mossakowski <gustaf@koenige.org>
2020-08-26 09:30:23 +0000

    add images template and page script for image gallery on a website
    git-svn-id: file:///Repos/modules/media/trunk@121 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ zzbrick_page/images.inc.php
@@ -0,0 +1,28 @@
+<?php 
+
+/**
+ * Zugzwang Project
+ * Output image gallery
+ *
+ * http://www.zugzwang.org/modules/media
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2014, 2017, 2019-2020 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/** 
+ * image gallery
+ * 
+ * @param array $params (HTML-Code, if value will be returned)
+ * @param array $page
+ * @return string $text
+ */
+function page_images($params, $page) {
+	if (empty($page['media']['images'])) return '';
+	array_shift($page['media']['images']);
+	if (empty($page['media']['images'])) return '';
+	$text = wrap_template('images', $page['media']['images']);
+	return $text;
+}
