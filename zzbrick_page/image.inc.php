6381e9f6e9f0026a0d785d9a07b80385b9c767cd
Gustaf Mossakowski <gustaf@koenige.org>
2021-01-18 13:24:47 +0000

    image: check against page status, not empty page
    git-svn-id: file:///Repos/modules/media/trunk@143 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_page/image.inc.php
+++ zzbrick_page/image.inc.php
@@ -7,7 +7,7 @@
  * http://www.zugzwang.org/modules/media
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2020 Gustaf Mossakowski
+ * @copyright Copyright © 2020-2021 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -23,7 +23,7 @@ function page_image(&$params, &$page) {
 	global $zz_setting;
 	require_once $zz_setting['lib'].'/zzbrick/request.inc.php';
 
-	if (empty($page)) {
+	if (empty($page['status'])) {
 		$text = '%%% page image '.implode(' ', $params).' %%%';
 	} elseif (!empty($page['media'])) {
 		array_unshift($params, 'image');
