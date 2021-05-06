13684d2f2970af6d21187780ca1a7af8dc009583
Gustaf Mossakowski <gustaf@koenige.org>
2021-05-06 08:23:27 +0000

    add setting embed_privacy[EMBED] for a custom text for an embed in the privacy settings; create an lowercase identifier for each embed with wrap_filename()
    git-svn-id: file:///Repos/modules/media/trunk@158 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_request/embedprivacy.inc.php
+++ zzbrick_request/embedprivacy.inc.php
@@ -1,13 +1,14 @@
 <?php 
 
 /**
- * Zugzwang Project
+ * media module
  * Privacy settings for embeds
  *
- * http://www.zugzwang.org/modules/media
+ * https://www.zugzwang.org/modules/media
+ * Part of »Zugzwang Project«
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2020 Gustaf Mossakowski
+ * @copyright Copyright © 2020-2021 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -25,8 +26,10 @@ function mod_media_embedprivacy($params) {
 	foreach (array_keys($zz_setting['embed']) as $embed) {
 		$data[] = [
 			'type' => $embed,
-			'selected' => in_array(strtolower($embed), $selected) ? true: false,
-			$embed => true
+			'identifier' => strtolower(wrap_filename($embed)),
+			'selected' => in_array(strtolower(wrap_filename($embed)), $selected) ? true: false,
+			$embed => true,
+			'privacy' => !empty($zz_setting['embed_privacy'][$embed]) ? wrap_text($zz_setting['embed_privacy'][$embed]) : ''
 		];
 	}
 	if (!$data)
