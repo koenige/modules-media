0fbfc9c443583fbd088b23586421c919db2cfab1
Gustaf Mossakowski <gustaf@koenige.org>
2021-10-18 14:43:05 +0000

    media: fix explanation text for field 'alternative_text'
    git-svn-id: file:///Repos/modules/media/trunk@166 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_tables/media.php
+++ zzbrick_tables/media.php
@@ -189,7 +189,7 @@ $zz['fields'][3]['typo_cleanup'] = true;
 
 $zz['fields'][17]['field_name'] = 'alternative_text';
 $zz['fields'][17]['type'] = 'text';
-$zz['fields'][17]['explanation'] = 'optional content of `alt`-attribute, for screen reader software';
+$zz['fields'][17]['explanation'] = 'optional content of `alt`-attribute for images, for screen reader software';
 $zz['fields'][17]['hide_in_list'] = true;
 
 // additional fields
