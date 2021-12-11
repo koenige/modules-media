41d00a9ed4004682513b4375f4d1e8419f2d2bf8
Gustaf Mossakowski <gustaf@koenige.org>
2021-12-11 11:40:55 +0000

    media.title: activate 'typo_remove_double_spaces'
    git-svn-id: file:///Repos/modules/media/trunk@180 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_tables/media.php
+++ zzbrick_tables/media.php
@@ -181,6 +181,7 @@ $zz['fields'][2]['class'] = 'legend block480a';
 $zz['fields'][2]['if'][3]['title'] = 'Code';
 $zz['fields'][2]['if'][3]['type'] = 'write_once';
 $zz['fields'][2]['typo_cleanup'] = true;
+$zz['fields'][2]['typo_remove_double_spaces'] = true;
 
 $zz['fields'][3]['field_name'] = 'description';
 $zz['fields'][3]['type'] = 'memo';
