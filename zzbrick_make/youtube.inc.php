7e7afdbc4d936f845fc6648ff320904d858f220f
Gustaf Mossakowski <gustaf@koenige.org>
2022-07-18 14:11:59 +0000

    youtube: allow to add main_medium_id via settings main_medium_id=
    git-svn-id: file:///Repos/modules/media/trunk@193 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_make/youtube.inc.php
+++ zzbrick_make/youtube.inc.php
@@ -18,7 +18,7 @@
  *
  * @param array $params
  */
-function mod_media_make_youtube($params) {
+function mod_media_make_youtube($params, $settings = []) {
 	global $zz_setting;
 
 	$meta = mf_media_get_embed_youtube($params[0]);
@@ -27,19 +27,23 @@ function mod_media_make_youtube($params) {
 	// add medium
 	$values = [];
 	$values['action'] = 'insert';
-	$sql = 'SELECT medium_id FROM media
-		WHERE filetype_id = %d
-		AND filename = "%s"';
-	$sql = sprintf($sql
-		, wrap_filetype_id('folder')
-		, $zz_setting['embed_path_youtube']
-	);
 	$values['GET']['add']['filetype_id'] = wrap_filetype_id('youtube');
-	$values['POST']['main_medium_id'] = wrap_db_fetch($sql, '', 'single value');
+	if (empty($settings['main_medium_id'])) {
+		$sql = 'SELECT medium_id FROM media
+			WHERE filetype_id = %d
+			AND filename = "%s"';
+		$sql = sprintf($sql
+			, wrap_filetype_id('folder')
+			, $zz_setting['embed_path_youtube']
+		);
+		$values['POST']['main_medium_id'] = wrap_db_fetch($sql, '', 'single value');
+	} else {
+		$values['POST']['main_medium_id'] = $settings['main_medium_id'];
+	}
 	$values['POST']['title'] = $params[0];
 	$values['POST']['source'] = 'YouTube';
 	$values['POST']['published'] = 'yes';
-	$values['POST']['filename'] = sprintf('%s/%s', $zz_setting['embed_path_youtube'], $params[0]);
+	$values['POST']['filename'] = $params[0];
 	$ops = zzform_multi('media', $values);
 	if (!$ops['id']) {
 		wrap_error(sprintf('Could not add YouTube Video %s.', $params[0]));
