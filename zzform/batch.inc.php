720b68ebfbe9d516fba730c4d4d93149b09411c2
Gustaf Mossakowski <gustaf@koenige.org>
2024-04-11 06:51:27 +0000

    add mf_media_folder(), returns medium_id for folder, or, if folder does not exists, creates it
    git-svn-id: file:///Repos/modules/media/trunk@334 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ zzform/batch.inc.php
@@ -0,0 +1,49 @@
+<?php 
+
+/**
+ * media module
+ * batch functions
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/media
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2024 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/**
+ * get folder medium_id
+ * create folder if it does not exist
+ *
+ * @param string $folder
+ * @return int
+ */
+function mf_media_folder($identifier) {
+	if ($medium_id = wrap_id('folders', $identifier, 'check')) return $medium_id;
+
+	// get all folders
+	$folders = explode('/', $identifier);
+	$paths = [];
+	$path = '';
+	foreach ($folders as $folder) {
+		$path = sprintf('%s%s/', $path, $folder);
+		$paths[$folder] = rtrim($path, '/');
+	}
+	
+	// check if folders exist
+	$main_medium_id = '';
+	foreach ($paths as $folder => $path) {
+		$medium_id = wrap_id('folders', $path, 'check');
+		if (!$medium_id) {
+			$line = [
+				'main_medium_id' => $main_medium_id,
+				'title' => $folder,
+				'filetype_id' => wrap_id('filetypes', 'folder')
+			];
+			$main_medium_id = $medium_id = zzform_insert('media', $line);
+		}
+	}
+	return $medium_id;
+}
