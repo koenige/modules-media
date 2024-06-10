04a5f1c6153bb4fbc864f087d9c36b570e694ed2
Gustaf Mossakowski <gustaf@koenige.org>
2024-06-10 21:40:54 +0000

    batch: if folder is numeric, add value as sequence in mf_media_folder()
    git-svn-id: file:///Repos/modules/media/trunk@338 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzform/batch.inc.php
+++ zzform/batch.inc.php
@@ -40,7 +40,8 @@ function mf_media_folder($identifier) {
 			$line = [
 				'main_medium_id' => $main_medium_id,
 				'title' => $folder,
-				'filetype_id' => wrap_id('filetypes', 'folder')
+				'filetype_id' => wrap_id('filetypes', 'folder'),
+				'sequence' => is_numeric($new_folder) ? $new_folder : ''
 			];
 			$main_medium_id = $medium_id = zzform_insert('media', $line);
 		}
